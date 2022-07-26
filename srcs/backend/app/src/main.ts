2e9df9bd6389c1654aae2816de17740774db9314
fleger42 <florianleger91120@gmail.com>
2022-07-26 07:59:21 +0200

    build: added CORS to the backend

--- srcs/backend/app/src/main.ts
+++ srcs/backend/app/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors=require("cors");
+  const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+  }
+
+  app.use(cors(corsOptions)) // Use this after the variable declaration
   await app.listen(process.env.BACK_ENV_PORT);
 }
 
