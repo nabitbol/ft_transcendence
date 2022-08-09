11085fa748cb01fcab0d0ab39df6dde18fa64104
pacorrei <paul13.correia@gmail.com>
2022-08-09 18:17:02 +0200

    login 42

--- srcs/backend/app/src/auth/auth.controller.ts
+++ srcs/backend/app/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Get, Res, Query } from '@nestjs/common';
 import { RegisterDto } from 'src/dto/register.dto';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
@@ -7,10 +7,28 @@ import { LocalAuthGuard } from './local-auth.guard';
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
+	@Get('login/42')
+	RedirApi(@Res() response)
+	{
+		response.writeHead(302,
+			{Location: process.env.API_URL}
+		  );
+		response.end();
+	}
+
+	@Get('login/api')
+	async GetAccessToken(@Query('code') QueryParams)
+	{	
+		const access_token = await this.authService.postApi(QueryParams);
+		await this.authService.loginApi(access_token);
+		//console.log(token);
+		//need further implementation
+	}
+
 	@UseGuards(LocalAuthGuard)
 	@Post('login')
-	async login(@Request() req) {
-		return this.authService.login(req.user);
+	async Login(@Request() req) {
+		return await this.authService.login(req.user);
 	}
 
 	@Post('register')
--- srcs/backend/app/src/auth/auth.service.ts
+++ srcs/backend/app/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserDto } from 'src/dto/user.dto';
 import { RegisterDto } from 'src/dto/register.dto';
@@ -6,6 +6,10 @@ import { User } from 'src/entity/user.entity';
 import { Repository, UpdateResult } from 'typeorm';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { HttpService } from '@nestjs/axios';
+import axios from "axios";
+import { firstValueFrom, map } from 'rxjs';
+import { response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -14,6 +18,7 @@ export class AuthService {
 		@InjectRepository(User)
 		private userRepository: Repository<User>,
 		private usersService: UserService,
+		private readonly httpService: HttpService,
 		private jwtService: JwtService,
 	) {}
 
@@ -47,5 +52,41 @@ export class AuthService {
 		await this.userRepository.save(user);
 		const { user_password, user_status, user_JWT, ...retUserDto } = user;
 		return (retUserDto);
+	}
+
+	async postApi(code : string)
+	{
+		const grant_type = 'authorization_code';
+		const client_id = process.env.CLIENT_ID
+		const client_secret = process.env.CLIENT_SECRET
+		const base_url = 'https://api.intra.42.fr/oauth/token';
+		const redirect_uri = 'http://localhost:3000/auth/login/api'
+
+		return axios
+      .post(base_url, {
+		grant_type,
+        client_id,
+		client_secret,
+		code,
+		redirect_uri
+      })
+      .then(response => {
+        return response.data.access_token;
+      });
+	}
+	async loginApi(access_token : string)
+	{
+		const base_url = 'https://api.intra.42.fr/v2/me';
+
+		const data = await firstValueFrom(
+			this.httpService.get(base_url, {headers: { Authorization: `Bearer ${access_token}` }}).pipe(
+				map((response) => {
+					return response.data
+				}),
+			),
+		);
+		const email = data.email;
+		const username = data.login;
+		
 	}
 }
