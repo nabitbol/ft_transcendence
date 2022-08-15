d3b4f47722cf6385cc37767410f12865930e1db0
nabitbol <noam1.abitbol@gmail.com>
2022-08-15 15:49:53 +0200

    feat: added a test packages to test the monorepository

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-
-bootstrap();
