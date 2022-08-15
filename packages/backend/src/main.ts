95929a6586da36a4e868b9488ef4902b158df366
nabitbol <noam1.abitbol@gmail.com>
2022-08-15 15:35:51 +0200

    fix: removed the export and call the bootstrap function directly in main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-export default async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+bootstrap();
