d6b26c74cb4ae50ce426890aed4d109b41c9aba1
nabitbol <noam1.abitbol@gmail.com>
2022-09-24 12:22:53 +0200

    feat: seted up the api documentation using swagger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,18 +3,30 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+
+  const config = new DocumentBuilder()
+    .setTitle("ft-transcendence API")
+    .setDescription("The ft-transcendence API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("test", app, document);
+
+  const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
