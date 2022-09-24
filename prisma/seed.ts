88d20cf7dd331ab95dc9516d661b745c99a34e3e
nabitbol <noam1.abitbol@gmail.com>
2022-09-24 22:13:13 +0200

    feat(db): added the ability to seed the databse with a exemple of seed

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type UserType = {
+  name: string;
+  email: string;
+  password: string;
+  image: string;
+  doubleAuth?: boolean | undefined;
+  wins?: number | undefined;
+  losses?: number | undefined;
+  draw?: number | undefined;
+  level?: number | undefined;
+  userRankId?: string | undefined;
+};
+
+const User: UserType = {
+  name: "Demo",
+  email: "Demo@mail.com",
+  password: "Demo",
+  image: "DemoString",
+};
+
+const main = async () => {
+  await prisma.user.create({ data: User });
+};
+
+main()
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
