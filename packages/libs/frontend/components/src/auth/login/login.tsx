63f2813925159173c871b18787d3b148f3099605
fleger42 <florianleger91120@gmail.com>
2022-10-05 08:46:17 +0200

    wip: fixing react error with old form

--- packages/libs/frontend/components/src/auth/login/login.tsx
+++ packages/libs/frontend/components/src/auth/login/login.tsx
@@ -1,108 +1,74 @@
 import classes from "./login.module.css";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import * as Service from "@ft-transcendence/libs-frontend-services";
+import { useForm } from "react-hook-form";
+import { AuthReq } from "@ft-transcendence/libs-frontend-services";
 
 type LoginProps = {
   activateTwoFaForm: () => void;
 };
 
 const Login: React.FC<LoginProps> = (props: LoginProps) => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate();
-  const form = useRef<any>();
-  const checkBtn = useRef<any>();
-  const [user_pseudo, setUserPseudo] = useState("");
-  const [user_password, setUserPassword] = useState("");
   const [message, setMessage] = useState("");
-  const onChangeUserPseudo = (event: any) => {
-    const user_pseudo = event.currentTarget["value"];
-    setUserPseudo(user_pseudo);
-  };
 
-  const onChangeUserPassword = (event: any) => {
-    const password = event.currentTarget["value"];
-    setUserPassword(password);
-  };
-
-  const handleLogin = (event: any) => {
-    event.preventDefault();
+  const handleLogin = (data) => {
     setMessage("");
-    form.current.validateAll();
-    if (checkBtn.current.context._errors.length === 0) {
-      Service.AuthReq.login(user_pseudo, user_password).then(
-        () => {
-          const user_data = localStorage.getItem("userdata");
-          if (user_data) {
-            const user = JSON.parse(user_data);
-            if (user && user.user_TwoFa_on) props.activateTwoFaForm();
-          } else {
-            navigate("/home");
-            window.location.reload();
-          }
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-          setMessage(resMessage);
+    AuthReq.login(data.user_pseudo, data.user_password).then(
+      () => {
+        const user_data = localStorage.getItem("userdata");
+        if (user_data) {
+          const user = JSON.parse(user_data);
+          if (user && user.user_TwoFa_on)
+            props.activateTwoFaForm();
         }
-      );
-    }
-  };
-
+        else {
+          navigate("/home");
+          window.location.reload();
+        }
+      },
+      (error) => {
+        const resMessage =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setMessage(resMessage);
+      }
+    );
+  }
+  
   return (
-    <Form className={classes["login_form"]} onSubmit={handleLogin} ref={form}>
-      <div className="form-group">
-        <label className={classes["login_label"]} htmlFor="user_pseudo">
-          Username
-        </label>
-        <Input
-          type="text"
-          className={classes["login_input"]}
-          name="user_pseudo"
-          value={user_pseudo}
-          onChange={onChangeUserPseudo}
-          validations={[Service.vrequired]}
-        />
-      </div>
+    <div className={classes.login_form}>
+    <span className={classes.login_span}>Login</span>
+    <form onSubmit={handleSubmit(handleLogin)}>
+      <input placeholder="Username" type="text"
+            className={errors.user_name ? classes.login_input_red : classes.login_input}
+            {...register("user_name", {
+              required: true
+            })}
+      />
 
-      <div className="form-group">
-        <label className={classes["login_label"]} htmlFor="user_password">
-          Password
-        </label>
-        <Input
-          type="password"
-          className={classes["login_input"]}
-          name="user_password"
-          value={user_password}
-          onChange={onChangeUserPassword}
-          validations={[Service.vrequired]}
-        />
-      </div>
-
-      <div className="form-group">
-        <button className={classes["login_btn"]}>
-          <span>Login</span>
-        </button>
-      </div>
+      <input placeholder="Password" type="text"
+        className={errors.user_password ? classes.login_input_red : classes.login_input}
+        {...register("user_password", {
+          required: true
+        })}
+      />
 
       {message && (
-        <div className="form-group">
           <div className="alert alert-danger" role="alert">
             {message}
           </div>
-        </div>
       )}
 
-      <CheckButton style={{ display: "none" }} ref={checkBtn} />
-    </Form>
+      <input type="submit" className={classes.login_btn} />
+    </form>
+  </div>
   );
 };
+
 
 export { Login };
