65f4d85c4c819b0514eb011d62392c104c7b6019
nabitbol <noam1.abitbol@gmail.com>
2022-10-12 22:13:38 +0200

    wip chat websocket gateway

--- packages/libs/backend/chat/src/chat.gateway.ts
+++ packages/libs/backend/chat/src/chat.gateway.ts
@@ -1,10 +1,25 @@
-import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { UseGuards } from "@nestjs/common";
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+} from "@nestjs/websockets";
 import { Socket } from "dgram";
 
 @WebSocketGateway(8080, { cors: { origin: "https://hoppscotch.io" } })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage("message")
-  handleMessage(client: any, payload: any): string {
+  handleMessage(client: Socket, payload: any): string {
     return "Hello world!";
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(client + "connected");
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(client + "disconnected");
+    client.disconnect();
   }
 }
