b605db31edb8d7ad6d9954b471097a78da9cc746
nabitbol <noam1.abitbol@gmail.com>
2022-10-19 11:07:49 +0200

    fix(backend): fixed type issue due to unexported message tyep

--- packages/libs/shared/types/src/index.ts
+++ packages/libs/shared/types/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/user.types";
 export * from "./lib/match.types";
+export * from "./lib/message.types";
 export * from "./lib/twofa.types";
 export * from "./lib/room.types";
 export * from "./lib/jwt.types";
