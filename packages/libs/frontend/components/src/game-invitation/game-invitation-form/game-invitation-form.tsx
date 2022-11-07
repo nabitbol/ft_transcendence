76dae0c8eacf254ca8bfe431ea20630fc5c4079f
fleger42 <florianleger91120@gmail.com>
2022-11-07 20:42:32 +0100

    fix: bug where accepting the invitation was removing the lobby

--- packages/libs/frontend/components/src/game-invitation/game-invitation-form/game-invitation-form.tsx
+++ packages/libs/frontend/components/src/game-invitation/game-invitation-form/game-invitation-form.tsx
@@ -4,18 +4,26 @@ import classes from "./game-invitation-form.module.css";
 type GameInvitationForm = {
   acceptInvitation: () => void;
   closeModal: () => void;
+  cancelInvitation: () => void;
 };
 
 const GameInvitationForm: React.FC<GameInvitationForm> = (props: GameInvitationForm) => {
+
+  const closeModal = () => {
+    props.cancelInvitation();
+    props.closeModal();
+  };
+  
   return (
     <div>
-      <Backdrop closeBackdrop={props.closeModal} />
+      <Backdrop closeBackdrop={closeModal} />
       <div className={classes['game_invitation_form']}>
         <span className={classes['game_invitation_span']}>
           You have been invited to play
         </span>
         <div className={classes['game_invitation_button_holder']}>
-          <button className={classes['game_invitation_btn']} onClick={props.closeModal}>
+          <button className={classes['game_invitation_btn']}
+            onClick={closeModal}>
             Refuse
           </button>
           <button
